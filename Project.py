d853b94fce9f4ad2c574fda9b5900ef832a4afb4
anagramiccoder <jdvidad31@gmail.com>
2022-06-02 18:56:22 +0800

    fixed divisor

--- Project.py
+++ Project.py
@@ -20,7 +20,7 @@ def senddata(path,ip_receiver,port_receiver, port_sender, uid,size=-1):
     data=datafile.readline()
     datafile.close()
     if size==-1:
-        size=ceil(len(data)/10)
+        size=ceil(len(data)//10)
     print(size,len(data))
     client.sendto(msg.encode(), (ip_receiver,port_receiver))
     transid, addr = client.recvfrom(4096)
