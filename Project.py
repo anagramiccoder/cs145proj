859075d2258b9ef748a39b9134a51d6aa5d431bf
anagramiccoder <jdvidad31@gmail.com>
2022-06-02 15:41:47 +0800

    added timeout 10s

--- Project.py
+++ Project.py
@@ -23,7 +23,6 @@ def senddata(path,ip_receiver,port_receiver, port_sender, uid,size=-1):
     client.sendto(msg.encode(), (ip_receiver,port_receiver))
     transid, addr = client.recvfrom(4096)
     exectime=time.perf_counter()
-    client.settimeout(ceil(exectime-sendtime)+1)
     #assume within 90 seconds
     if size==-1:
         size=(len(data)//90)*(ceil(exectime-sendtime)+1)*10
