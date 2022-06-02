b6bf443309f5b8a03fdeca82e4a4de87ace8218d
anagramiccoder <jdvidad31@gmail.com>
2022-06-03 01:40:47 +0800

    added max possible

--- autoadj.py
+++ autoadj.py
@@ -54,7 +54,7 @@ def senddata(path,ip_receiver,port_receiver, port_sender, uid,size=-1):
     maxfound=False
     while i<=len(data):
         remaining=len(data)//size+bool(len(data)%size)
-        print("sending packet(packetsize:",size,"):",(counter),"/",remaining)
+        print("sending packet(packetsize:",size," max possible:",msize,"):",(counter),"/",remaining)
         partdata=data[i:i+size]
         a=int(not ((i+size)<len(data)))
         msg=f"ID{uid}SN{counter:07d}TXN{tid}LAST{a}{partdata}"
