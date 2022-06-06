baf0d9e30571bae7167a3dd2456effa1ed3fdb5f
anagramiccoder <jdvidad31@gmail.com>
2022-06-06 15:25:37 +0800

    tshark

--- tshark.py
+++ tshark.py
@@ -1,13 +1,15 @@
 
 import os
+import sys
 from datetime import datetime
 
 # Run tshark for 120 seconds and save the output to a file
-def run_tshark():
+def run_tshark(file_name=f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"):
     # set filename to the current date time
-    file_name = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+    print(file_name)
     os.system(f"touch tfiles/{file_name}.pcap")
     os.system(f"chmod o=rw tfiles/{file_name}.pcap")
     os.system(f"sudo tshark -a duration:120 -w tfiles/{file_name}.pcap")
 
-run_tshark()
+fname=sys.argv[1]
+run_tshark(fname)
